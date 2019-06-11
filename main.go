f8bcb7fc2e09764bb79eb771ac6b94286c281859
Yuji TANI <yuji.tani.jp@gmail.com>
2019-06-12 03:42:33 +0900

    一覧画面実装

--- main.go
+++ main.go
@@ -1,21 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"google.golang.org/appengine"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+const (
+	DBMS     string = "mysql"
+	USER     string = "*****"
+	PASS     string = "*****"
+	PROTOCOL string = "tcp(27.133.130.174:3306)"
+	DBNAME   string = "diary_app"
+	CONNECT         = USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+)
+
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
+
+// DB初期化
+func dbinit() {
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic("データベース開けず！（dbInit）")
+	}
+
+	db.AutoMigrate(&Todo{})
+	//db.CreateTable(&Todo{})
+	defer db.Close()
+
+}
+
+func dbGetAll() []Todo {
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic("データベース開けず！（dbGetAll）")
+	}
+	var todos []Todo
+	//db.Order("created_at desc").Find(&todos)
+	db.Find(&todos)
+	db.Close()
+
+	fmt.Println("Status = " + todos[0].Status)
+	fmt.Println("Text = " + todos[0].Text)
+	fmt.Println("Status = " + todos[1].Status)
+	fmt.Println("Text = " + todos[1].Text)
+
+	return todos
+
+}
+
 func main() {
+
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
-	data := "Hello Go/Gin!!"
+	dbinit()
 
+	// Index
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{"data": data})
-	})
+		todos := dbGetAll()
 
-	appengine.Main()
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
+	})
 
 	router.Run(":9090")
 
